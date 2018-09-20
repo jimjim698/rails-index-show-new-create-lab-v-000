e3cc315de88d7e851fac279652963e6596fd1725
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-20 23:14:14 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,9 +1,10 @@
 class CouponsController < ApplicationController
 
   def new
-  end 
+  end
 
   def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
   end
 
 
